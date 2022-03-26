7cd2075b823dc67169c3531c96d0f81e9fea049d
rafael ferreira <contact@ferreirarafael.com>
2022-03-26 17:36:19 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(SiteController::class)->group(function(){
-    Route::get("/")->name("home");
+    Route::get("/","index")->name("home");
 });
