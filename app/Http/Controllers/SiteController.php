a4bd3a3b2698073f0f832505d95c89cbf8aa403a
rafael ferreira <contact@ferreirarafael.com>
2022-03-30 21:03:45 +0200

    .

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,27 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Stripe\Charge;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
 
 class SiteController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("site.welcome");
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+        Charge::create([
+            "amount" => 100 * 100,
+            "currency" => "usd",
+            "source" => $request->stripeToken,
+            "description" => "Test payment from tutsmake.com."
+        ]);
+
+        Session::flash('success', 'Payment successful!');
+
+        return back();
     }
 
-    public function about(){
+    public function about()
+    {
         return view("site.about");
     }
 
-    public function send(){
+    public function send()
+    {
         return view("site.send");
     }
 
-    public function identification(){
+    public function identification()
+    {
         return view("site.send_identification");
     }
 
-    public function payment(){
+    public function payment()
+    {
         return view("site.payment");
     }
 }
