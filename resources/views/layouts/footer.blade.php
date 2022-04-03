4c7a3b4028e715f5425dea3ec8f0f4b1792cf356
rafael ferreira <contact@ferreirarafael.com>
2022-04-03 15:14:16 +0200

    .

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -26,14 +26,12 @@
         <div class="footer-copyright pt-3 pt-lg-2 mt-2">
             <div class="row">
                 <div class="col-lg">
-                    <p class="text-center text-lg-start mb-2 mb-lg-0">Copyright &copy; 2022 <a href="#">Payyed</a>. All
+                    <p class="text-center text-lg-start mb-2 mb-lg-0">Copyright &copy; 2022 <a href="#">Rafael Ferreira</a>. All
                         Rights Reserved.</p>
                 </div>
                 <div class="col-lg d-lg-flex align-items-center justify-content-lg-end">
                     <ul class="nav justify-content-center">
-                        <li class="nav-item"> <a class="nav-link active" href="#">Security</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="#">Terms</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="#">Privacy</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="{{route("privacity")}}">Privacidade</a></li>
                     </ul>
                 </div>
             </div>
