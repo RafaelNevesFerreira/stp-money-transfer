60965ceedefe4e7a78f31a97a111c021afc65eac
rafael ferreira <contact@ferreirarafael.com>
2022-04-05 20:21:45 +0200

    .

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,15 +4,13 @@
             <div class="header-column justify-content-start">
                 <!-- Logo  ============================= -->
                 <div class="logo me-3">
-                    <a class="d-flex" href="{{route("home")}}"
-                        title="Payyed - HTML Template"><img
-                            src="{{asset("assets/images/logos.png")}}"
-                            alt="Payyed" /></a>
+                    <a class="d-flex" href="{{ route('home') }}" title="Payyed - HTML Template"><img
+                            src="{{ asset('assets/images/logos.png') }}" alt="Payyed" /></a>
                 </div>
                 <!-- Logo end -->
                 <!-- Collapse Button     ============================== -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#header-nav"> <span></span> <span></span> <span></span> </button>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#header-nav">
+                    <span></span> <span></span> <span></span> </button>
                 <!-- Collapse Button end -->
 
                 <!-- Primary Navigation        ============================== -->
@@ -22,25 +20,36 @@
                             <li><a href="{{ route('home') }}">Home</a></li>
                             <li><a href="{{ route('send') }}">Enviar</a> </li>
                             <li><a href="{{ route('about') }}">Sobre Nós</a></li>
-                            <li><a href="{{route("help")}}">Ajuda</a></li>
-                            <li><a href="{{route("blog")}}">Blog</a></li>
-                            <li><a href="{{route("contact")}}">Contato</a></li>
+                            <li><a href="{{ route('help') }}">Ajuda</a></li>
+                            <li><a href="{{ route('blog') }}">Blog</a></li>
+                            <li><a href="{{ route('contact') }}">Contato</a></li>
 
                         </ul>
                     </div>
                 </nav>
                 <!-- Primary Navigation end -->
             </div>
+
             <div class="header-column justify-content-end">
                 <!-- Login & Signup Link   ============================== -->
                 <nav class="login-signup navbar navbar-expand">
                     <ul class="navbar-nav">
-                        <li><a href="{{ route('login') }}">Login</a> </li>
-                        <li class="align-items-center h-auto ms-sm-3"><a class="btn btn-primary" href="{{route("register")}}">Sign Up</a></li>
+                        @guest
+                            <li><a href="{{ route('login') }}">Login</a> </li>
+                            <li class="align-items-center h-auto ms-sm-3"><a class="btn btn-primary"
+                                    href="{{ route('register') }}">Sign Up</a></li>
+                        @endguest
+                        @auth
+                            <li class="align-items-center h-auto ms-sm-3"><a class="btn btn-primary" href="{{ route('login') }}">DashBoard</a> </li>
+                        @endauth
                     </ul>
                 </nav>
                 <!-- Login & Signup Link end -->
             </div>
+
+
+
+
         </div>
     </div>
 </header>
