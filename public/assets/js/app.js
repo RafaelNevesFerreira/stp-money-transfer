d24b69ee020d53399958f872528976c747135a93
rafael ferreira <contact@ferreirarafael.com>
2022-04-06 17:19:33 +0200

    .

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -90,22 +90,6 @@
                 });
                 valor = parseFloat(valor.replace(".", ''))
 
-                if (valor >= 100 && valor <= 400) {
-                    var minha_tax = 27
-                } else if (valor > 400 && valor <= 800) {
-                    minha_tax = 50
-                } else if (valor > 800 && valor <= 1000) {
-                    minha_tax = 150
-                } else if (valor == 25) {
-                    minha_tax = 5;
-                } else if (valor < 25) {
-                    minha_tax = 3;
-                } else {
-                    minha_tax = 10;
-                }
-                var tax = valor * 0.030 + 0.3 + minha_tax
-
-                var total = valor + tax;
                 $("#taxas").text(tax.toFixed(2));
 
                 $("#total").text(total);
@@ -127,24 +111,6 @@
                 });
                 valor = parseFloat(valor.replace(".", ''))
 
-                if (valor >= 100 && valor <= 400) {
-                    var minha_tax = 27
-                } else if (valor > 400 && valor <= 800) {
-                    minha_tax = 50
-                } else if (valor > 800 && valor <= 1000) {
-                    minha_tax = 150
-                } else if (valor == 25) {
-                    minha_tax = 5;
-                } else if (valor < 25) {
-                    minha_tax = 3;
-                } else {
-                    minha_tax = 10;
-                }
-                var tax = valor * 0.030 + 0.3 + minha_tax
-
-                var total = valor + tax;
-                $("#taxas").text(tax.toFixed(2));
-                $("#total").text(total);
 
                 var valor_mueda = 29;
                 $("#recipientGets").val(formater.format(valor * valor_mueda));
