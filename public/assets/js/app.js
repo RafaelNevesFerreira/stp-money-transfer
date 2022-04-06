a9bcb1d5115bc8e0e781423a95ac33107f6467fd
rafael ferreira <contact@ferreirarafael.com>
2022-04-06 09:26:28 +0200

    .

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -43,9 +43,6 @@
         $("#youSendCurrency").change(function() {
             if ($(this).val() == "eur") {
                 $(".moeda_mudar").text("€")
-
-            } else if ($(this).val() == "usd") {
-                $(".moeda_mudar").text("$")
                 var valor = $("#youSend").val()
 
                 var formater = new Intl.NumberFormat("fr-FR", {
@@ -73,13 +70,80 @@
                 $("#taxas").text(tax.toFixed(2));
                 $("#total").text(total);
 
-                valor_mueda = 23;
+                valor_mueda = 25;
                 $("#recipientGets").val(formater.format(valor * valor_mueda));
+
+                $(".moeda_mudar").text("$")
+
+            } else if ($(this).val() == "usd") {
+                var valor = $("#youSend").val()
+
+                var formater = new Intl.NumberFormat("fr-FR", {
+                    style: "currency",
+                    currency: "stn"
+                });
+                valor = parseFloat(valor.replace(".", ''))
+
+                if (valor >= 100 && valor <= 400) {
+                    var minha_tax = 27
+                } else if (valor > 400 && valor <= 800) {
+                    minha_tax = 50
+                } else if (valor > 800 && valor <= 1000) {
+                    minha_tax = 150
+                } else if (valor == 25) {
+                    minha_tax = 5;
+                } else if (valor < 25) {
+                    minha_tax = 3;
+                } else {
+                    minha_tax = 10;
+                }
+                var tax = valor * 0.030 + 0.3 + minha_tax
+
+                var total = valor + tax;
+                $("#taxas").text(tax.toFixed(2));
+                $(".moeda_mudar").text("$")
+
+                // $("#total").text(total);
+
+                // valor_mueda = 22;
+
+                // $("#recipientGets").val(formater.format(valor * valor_mueda));
 
 
 
             } else {
                 $(".moeda_mudar").text("£")
+                var valor = $("#youSend").val()
+
+                var formater = new Intl.NumberFormat("fr-FR", {
+                    style: "currency",
+                    currency: "stn"
+                });
+                valor = parseFloat(valor.replace(".", ''))
+
+                if (valor >= 100 && valor <= 400) {
+                    var minha_tax = 27
+                } else if (valor > 400 && valor <= 800) {
+                    minha_tax = 50
+                } else if (valor > 800 && valor <= 1000) {
+                    minha_tax = 150
+                } else if (valor == 25) {
+                    minha_tax = 5;
+                } else if (valor < 25) {
+                    minha_tax = 3;
+                } else {
+                    minha_tax = 10;
+                }
+                var tax = valor * 0.030 + 0.3 + minha_tax
+
+                var total = valor + tax;
+                $("#taxas").text(tax.toFixed(2));
+                $("#total").text(total);
+
+                valor_mueda = 29;
+                $("#recipientGets").val(formater.format(valor * valor_mueda));
+
+                $(".moeda_mudar").text("$")
             }
         })
         var formater = new Intl.NumberFormat("stn", {
