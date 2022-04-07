2a029eed34387cc091166078f33e9d657019976f
rafael ferreira <contact@ferreirarafael.com>
2022-04-07 12:07:15 +0200

    .

--- app/Http/Controllers/AbonementController.php
+++ app/Http/Controllers/AbonementController.php
@@ -6,8 +6,42 @@ use Illuminate\Http\Request;
 
 class AbonementController extends Controller
 {
-    public function pagar_em_3_vezes(Request $request){
+    public function pagar_em_3_vezes(Request $request)
+    {
         dd($request->all());
     }
 
+    public function pagar_em_2_vezes(Request $request)
+    {
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId'
+        );
+
+        $costumer = $stripe->customers->create([
+            'email' => session("email"),
+            'name' => session("name"),
+            'phone' => session("phone_number"),
+        ]);
+
+        $card = $stripe->customers->createSource(
+            $costumer->id,
+            [
+                'source' => [
+                    "object" => "card",
+                    "number" => $request->card_no,
+                    "exp_month" => $request->exp_month,
+                    "exp_year" => $request->exp_year,
+                    "cvc" => $request->cvc,
+                ],
+            ],
+        );
+
+        $stripe->subscriptions->create([
+            'customer' => $costumer->id,
+            'items' => [
+                ['price' => 'price_1KlcEUFzWXjclIq0kPWaHzXs'],
+            ],
+        ]);
+        dd($request->all());
+    }
 }
--- app/Http/Controllers/PaymentTipeController.php
+++ app/Http/Controllers/PaymentTipeController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PaymentTipeController extends Controller
 {
-    public function __construct(){
-
+    public function __construct(public AbonementController $abonement)
+    {
     }
-    
 
-    public function verificar_condição_de_pagamento(Request $request){
-        if ($request->has("pagar_em_prestacoes")) {
-            dd($request->all());
+
+    public function verificar_condição_de_pagamento(Request $request)
+    {
+        if ($request->has("pagar_em_prestacoes") && Auth::check()) {
+            if ($request->numero_prestacoes == "3") {
+                $this->abonement->pagar_em_3_vezes($request);
+            } else {
+                $this->abonement->pagar_em_2_vezes($request);
+            }
+        }else{
+            return redirect()->back()->with("error","para pagar em prestaç~es deve se registrar");
         }
     }
 }
