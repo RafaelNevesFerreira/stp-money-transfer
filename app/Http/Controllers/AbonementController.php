6ff300e3b384828b1f8650e6313a528d2414f734
rafael ferreira <contact@ferreirarafael.com>
2022-04-08 17:03:44 +0200

    .

--- app/Http/Controllers/AbonementController.php
+++ app/Http/Controllers/AbonementController.php
@@ -10,8 +10,8 @@ use App\Repositories\Contracts\PlansRepositoryInterface;
 class AbonementController extends Controller
 {
 
-    public function __construct(public PlansRepositoryInterface $plans, public PaymentController $payment){
-
+    public function __construct(public PlansRepositoryInterface $plans, public PaymentController $payment)
+    {
     }
     public function pagar_em_3_vezes(Request $request)
     {
@@ -25,52 +25,55 @@ class AbonementController extends Controller
             'sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId'
         );
 
-        // $have_a_plan = $this->plans->ifExist();
+        $have_a_plan = $this->plans->ifExist();
 
 
-        // $costumer = $stripe->customers->create([
-        //     'email' => session("email"),
-        //     'name' => session("name"),
-        //     'phone' => session("phone_number"),
-        // ]);
+        $costumer = $stripe->customers->create([
+            'email' => session("email"),
+            'name' => session("name"),
+            'phone' => session("phone_number"),
+        ]);
 
-        // $card = $stripe->customers->createSource(
-        //     $costumer->id,
-        //     [
-        //         'source' => [
-        //             "object" => "card",
-        //             "number" => $request->card_no,
-        //             "exp_month" => $request->exp_month,
-        //             "exp_year" => $request->exp_year,
-        //             "cvc" => $request->cvc,
-        //         ],
-        //     ],
-        // );
+        $card = $stripe->customers->createSource(
+            $costumer->id,
+            [
+                'source' => [
+                    "object" => "card",
+                    "number" => $request->card_no,
+                    "exp_month" => $request->exp_month,
+                    "exp_year" => $request->exp_year,
+                    "cvc" => $request->cvc,
+                ],
+            ],
+        );
 
-        // $price = $stripe->subscriptions->create([
-        //     'customer' => $costumer->id,
-        //     'items' => [
-        //         ['price' => 'price_1KlcEUFzWXjclIq0kPWaHzXs'],
-        //     ],
-        // ]);
+        $subscription = $stripe->subscriptions->create([
+            'customer' => $costumer->id,
+            'items' => [
+                ['price' => 'price_1KlcEUFzWXjclIq0kPWaHzXs'],
+            ],
+        ]);
 
-        // $price = $stripe->paymentIntents->confirm(
-        //     'pi_3KmI13FzWXjclIq004YXuwJ8',
-        //     ['payment_method' => 'pm_card_visa']
-        //   );
 
+        $name = session("name");
         $client = $stripe->customers->search([
-            'query' => 'name:rafel ',
-          ]);
-        $price = $stripe->paymentIntents->search([
-            'query' => 'customer:\'cus_LTETURg3heiPll\' ',
-          ]);
+            'query' => "name:'" . $name . "'",
+        ]);
 
-          dd($price->data);
+
+        $price = $stripe->paymentIntents->search([
+            'query' => "customer:'" . $client->data[0]->id . "'",
+        ]);
+
+        $payment = $stripe->paymentIntents->confirm(
+            $price->data[0]->id,
+            ['payment_method' => 'pm_card_visa']
+        );
+
+
+
+        dd($client);
 
         return $this->payment->store($request);
-
-
-
     }
 }
