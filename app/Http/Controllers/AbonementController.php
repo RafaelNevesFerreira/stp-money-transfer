9c7dee2b1a349a1d2c82c3127b087725860549b3
rafael ferreira <contact@ferreirarafael.com>
2022-04-08 16:49:40 +0200

    .

--- app/Http/Controllers/AbonementController.php
+++ app/Http/Controllers/AbonementController.php
@@ -25,34 +25,48 @@ class AbonementController extends Controller
             'sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId'
         );
 
-        $have_a_plan = $this->plans->ifExist();
+        // $have_a_plan = $this->plans->ifExist();
 
 
-        $costumer = $stripe->customers->create([
-            'email' => session("email"),
-            'name' => session("name"),
-            'phone' => session("phone_number"),
-        ]);
+        // $costumer = $stripe->customers->create([
+        //     'email' => session("email"),
+        //     'name' => session("name"),
+        //     'phone' => session("phone_number"),
+        // ]);
 
-        $card = $stripe->customers->createSource(
-            $costumer->id,
-            [
-                'source' => [
-                    "object" => "card",
-                    "number" => $request->card_no,
-                    "exp_month" => $request->exp_month,
-                    "exp_year" => $request->exp_year,
-                    "cvc" => $request->cvc,
-                ],
-            ],
-        );
+        // $card = $stripe->customers->createSource(
+        //     $costumer->id,
+        //     [
+        //         'source' => [
+        //             "object" => "card",
+        //             "number" => $request->card_no,
+        //             "exp_month" => $request->exp_month,
+        //             "exp_year" => $request->exp_year,
+        //             "cvc" => $request->cvc,
+        //         ],
+        //     ],
+        // );
 
-        $stripe->subscriptions->create([
-            'customer' => $costumer->id,
-            'items' => [
-                ['price' => 'price_1KlcEUFzWXjclIq0kPWaHzXs'],
-            ],
-        ]);
+        // $price = $stripe->subscriptions->create([
+        //     'customer' => $costumer->id,
+        //     'items' => [
+        //         ['price' => 'price_1KlcEUFzWXjclIq0kPWaHzXs'],
+        //     ],
+        // ]);
+
+        // $price = $stripe->paymentIntents->confirm(
+        //     'pi_3KmI13FzWXjclIq004YXuwJ8',
+        //     ['payment_method' => 'pm_card_visa']
+        //   );
+
+        $client = $stripe->customers->search([
+            'query' => 'name:\'rafel\' ',
+          ]);
+        $price = $stripe->paymentIntents->search([
+            'query' => 'customer:\'cus_LTETURg3heiPll\' ',
+          ]);
+
+          dd($price->data);
 
         return $this->payment->store($request);
 
