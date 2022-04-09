bca4e31a104385c579268e81a8310b9cb0099390
rafael ferreira <contact@ferreirarafael.com>
2022-04-09 07:45:02 +0200

    .

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -13,10 +13,10 @@ class SiteController extends Controller
             'sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId'
           );
           $stripe->plans->create([
-            'amount' => 12000,
+            'amount' => 28000,
             'currency' => 'eur',
             'interval' => 'month',
-            'product' => 'price_1KlcEUFzWXjclIq0kPWaHzXs',
+            'product' => 'prod_LSXJFWphfFl1Cc',
           ]);
           dd("memes");
         return view("site.welcome");
