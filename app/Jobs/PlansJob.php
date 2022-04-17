dae1fd9082db976617dfd5d35433fedeb270d3f4
rafael ferreira <contact@ferreirarafael.com>
2022-04-17 19:36:14 +0200

    .

--- app/Jobs/PlansJob.php
+++ app/Jobs/PlansJob.php
@@ -89,6 +89,16 @@ class PlansJob implements ShouldQueue
             }
         } catch (\Throwable $th) {
             // PaimentFailed::dispatch($this->email)->delay(now()->addSecond(30));
+            $stripe = new \Stripe\StripeClient(
+                'sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId'
+            );
+            $link = $stripe->paymentLinks->create([
+                'line_items' => [
+                    [
+                        'price' => "price_1KlcEUFzWXjclIq0kPWaHzXs",
+                    ],
+                ],
+            ]);
             PaymentRequiresAction::dispatch($this->name, $link->url,$this->email)->delay(now()->addSecond(30));
 
         }
