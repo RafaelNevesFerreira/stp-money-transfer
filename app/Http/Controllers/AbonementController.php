7561239b04f28b01c112b3b503a7bcfb3c9f50ec
rafael ferreira <contact@ferreirarafael.com>
2022-04-22 19:20:48 +0200

    .

--- app/Http/Controllers/AbonementController.php
+++ app/Http/Controllers/AbonementController.php
@@ -47,52 +47,52 @@ class AbonementController extends Controller
 
 
             //atribui a varivael nome o valor do nome de quem envia o dinhiro
-            $name = session("name");
+            // $name = session("name");
 
             //prepara a query e faz uma pesquisa na apin do stripe para ver se existe algum cliente com esse nome
-            $client = $stripe->customers->search([
-                'query' => "name:'" . $name . "' ",
-            ]);
+            // $client = $stripe->customers->search([
+            //     'query' => "name:'" . $name . "' ",
+            // ]);
 
             //verifica se o cliente existe, caso não cria um novo cliente com os dados do emisor do dinheiro
-            if ($client->count() == 0) {
+            // if ($client->count() == 0) {
 
-                $stripe->customers->create([
-                    'email' => session("email"),
-                    'name' => session("name"),
-                    'phone' => session("phone_number"),
-                ]);
+                // $stripe->customers->create([
+                //     'email' => session("email"),
+                //     'name' => session("name"),
+                //     'phone' => session("phone_number"),
+                // ]);
                 //espera 15 segundos, o tempo nesecario para que o cliente seja cadastrdado no banco de dados do stripe
-                sleep(20);
-            }
+                // sleep(20);
+            // }
 
             //faz a pesquisa de novo, dessa vez para poder pegar o id do cliente
-            $client = $stripe->customers->search([
-                'query' => "name:'" . $name . "' ",
-            ]);
+            // $client = $stripe->customers->search([
+            //     'query' => "name:'" . $name . "' ",
+            // ]);
 
             //pega todos os cartões de credito usados pelo cliente
-            $card = $stripe->customers->allSources(
-                $client->data[0]->id,
-                ['object' => 'card']
-            );
+            // $card = $stripe->customers->allSources(
+            //     $client->data[0]->id,
+            //     ['object' => 'card']
+            // );
 
             //inicializa a variavel exit como sendo false, ou seja o cartão ainda não foi usado pelo cliente
-            $exist = false;
+            // $exist = false;
 
             //verifica se o cliente temcartões registrados, caso tenha, verifica se nos cartoes registrados os ultimos 4
             //numeros batem com os ultimos do cartão que sta sendo usado agora, caso sim a variavel exist recebera true
             //caso ainda não tenha o catão registrado, exist = false, e caso não tenha cartão alhum registrado, exist = false
-            if (count($card->data) > 0) {
-                for ($i = 0; $i < count($card->data); $i++) {
-                    if ($card->data[$i]["last4"] == substr($request->card_no, strlen($request->card_no) - 4)) {
-                        $exist = true;
-                        $card_id = $card->data[$i]["id"];
-                    }
-                }
-            } else {
-                $exist = false;
-            }
+            // if (count($card->data) > 0) {
+            //     for ($i = 0; $i < count($card->data); $i++) {
+            //         if ($card->data[$i]["last4"] == substr($request->card_no, strlen($request->card_no) - 4)) {
+            //             $exist = true;
+            //             $card_id = $card->data[$i]["id"];
+            //         }
+            //     }
+            // } else {
+            //     $exist = false;
+            // }
 
             //espera 5 segundos e depois adiciona o cartão criado como sendo default, para que dessa maneira o pagamento seja feito
             //com esse novo cartão registrado
@@ -106,29 +106,29 @@ class AbonementController extends Controller
 
 
             //verifica se a variavel exist = false, se sim então cria um novo cartão de credito ligado ao cliente
-            if (isset($exist) && $exist != true) {
-                $new_card = $stripe->customers->createSource(
-                    $client->data[0]->id,
-                    [
-                        'source' => [
-                            "object" => "card",
-                            "number" => $request->card_no,
-                            "exp_month" => $request->exp_month,
-                            "exp_year" => $request->exp_year,
-                            "cvc" => $request->cvc,
-                            "default_for_currency" => true,
-                        ],
-                    ],
-                );
+            // if (isset($exist) && $exist != true) {
+            //     $new_card = $stripe->customers->createSource(
+            //         $client->data[0]->id,
+            //         [
+            //             'source' => [
+            //                 "object" => "card",
+            //                 "number" => $request->card_no,
+            //                 "exp_month" => $request->exp_month,
+            //                 "exp_year" => $request->exp_year,
+            //                 "cvc" => $request->cvc,
+            //                 "default_for_currency" => true,
+            //             ],
+            //         ],
+            //     );
 
-                sleep(5);
+                // sleep(5);
                 //espera 5 segundos e depois adiciona o cartão criado como sendo default, para que dessa maneira o pagamento seja feito
                 //com esse novo cartão registrado
                 // $stripe->customers->update(
                 //     $client->data[0]->id,
                 //     ['default_source' => $new_card->id]
                 // );
-            }
+            // }
 
             $total = (session("total") / 100 * 20 + session("total")) /2;
 
