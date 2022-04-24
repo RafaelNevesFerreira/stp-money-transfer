2290e2cb1a3f5845f0c1e2a460fd30d3bc3bf07a
rafael ferreira <contact@ferreirarafael.com>
2022-04-24 11:07:57 +0200

    .

--- resources/views/profile/profille.blade.php
+++ resources/views/profile/profille.blade.php
@@ -1,38 +1,38 @@
 @extends("layouts.profile.app")
 @section('content')
     <!-- Content
-                  ============================================= -->
+                                      ============================================= -->
     <div id="content" class="py-4">
         <div class="container">
             <div class="row">
                 <!-- Left Panel
-                        ============================================= -->
+                                            ============================================= -->
                 @include('layouts.profile.left-painel')
                 <!-- Left Panel End -->
 
                 <!-- Middle Panel
-                        ============================================= -->
+                                            ============================================= -->
                 <div class="col-lg-9">
 
                     <!-- Recent Activity
-                          =============================== -->
+                                              =============================== -->
                     <div class="bg-white shadow-sm rounded py-4 mb-4">
-                        <h3 class="text-5 fw-400 d-flex align-items-center px-4 mb-4">Recent Activity</h3>
+                        <h3 class="text-5 fw-400 d-flex align-items-center px-4 mb-4">Atividades Recentes</h3>
 
                         <!-- Title
-                            =============================== -->
+                                                =============================== -->
                         <div class="transaction-title py-2 px-4">
                             <div class="row fw-00">
-                                <div class="col-2 col-sm-1 text-center"><span class="">Date</span></div>
-                                <div class="col col-sm-7">Description</div>
+                                <div class="col-2 col-sm-1 text-center"><span class="">Data</span></div>
+                                <div class="col col-sm-7">Destinatario</div>
                                 <div class="col-auto col-sm-2 d-none d-sm-block text-center">Status</div>
-                                <div class="col-3 col-sm-2 text-end">Amount</div>
+                                <div class="col-3 col-sm-2 text-end">Valor</div>
                             </div>
                         </div>
                         <!-- Title End -->
 
                         <!-- Transaction List
-                            =============================== -->
+                                                =============================== -->
 
                         @forelse ($transfers as $transfer)
                             <div class="transaction-list">
@@ -40,12 +40,22 @@
                                     data-bs-target="#transaction-detail">
                                     <div class="row align-items-center flex-row">
                                         <div class="col-2 col-sm-1 text-center">
-                                            <span class="d-block text-4 fw-300">{{$transfer->created_at->format("d")}}</span>
-                                            <span class="d-block text-1 fw-300 text-uppercase">{{$transfer->created_at->format("F")}}</span>
+                                            <span
+                                                class="d-block text-4 fw-300">{{ $transfer->created_at->format('d') }}</span>
+                                            <span
+                                                class="d-block text-1 fw-300 text-uppercase">{{ $transfer->created_at->format('F') }}</span>
                                         </div>
                                         <div class="col col-sm-7">
-                                            <span class="d-block text-4">{{$transfer->destinatary_name}}</span>
-                                            <span class="text-muted">Withdraw to Bank account</span>
+                                            <span class="d-block text-4">{{ $transfer->destinatary_name }}</span>
+                                            @switch($transfer->plan)
+                                                @case(1)
+                                                    <span class="text-muted">Pago em prestações</span>
+                                                @break
+
+                                                @case(0)
+                                                    <span class="text-muted">Debitado em cartão</span>
+                                                @break
+                                            @endswitch
                                         </div>
                                         <div class="col-auto col-sm-2 d-none d-sm-block text-center text-3">
                                             @switch($transfer->status)
@@ -70,20 +80,34 @@
 
                                         </div>
                                         <div class="col-3 col-sm-2 text-end text-4">
-                                            <span class="text-nowrap">{{$transfer->value_sended}}</span>
-                                            <span class="text-2 text-uppercase">(USD)</span>
+                                            @switch($transfer->currency)
+                                                @case('eur')
+                                                    <span class="text-nowrap">€ {{ $transfer->value_sended }}</span>
+                                                    <span class="text-2 text-uppercase">(EUR)</span>
+                                                @break
+
+                                                @case('usd')
+                                                    <span class="text-nowrap">$ {{ $transfer->value_sended }}</span>
+                                                    <span class="text-2 text-uppercase">(USD)</span>
+                                                @break
+
+                                                @case('gbp')
+                                                    <span class="text-nowrap">£ {{ $transfer->value_sended }}</span>
+                                                    <span class="text-2 text-uppercase">(GBP)</span>
+                                                @break
+                                            @endswitch
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                                @empty
+                            @empty
                             @endforelse
 
                         </div>
                         <!-- Transaction List End -->
 
                         <!-- Transaction Item Details Modal
-                                    =========================================== -->
+                                                                            =========================================== -->
                         <div id="transaction-detail" class="modal fade" role="dialog" aria-hidden="true">
                             <div class="modal-dialog modal-dialog-centered transaction-details" role="document">
                                 <div class="modal-content">
@@ -142,7 +166,7 @@
                         <!-- Transaction Item Details Modal End -->
 
                         <!-- View all Link
-                                    =============================== -->
+                                                                            =============================== -->
                         <div class="text-center mt-4"><a href="http://demo.harnishdesign.net/html/payyed/transactions.html"
                                 class="btn-link text-3">View all<i class="fas fa-chevron-right text-2 ms-2"></i></a></div>
                         <!-- View all Link End -->
