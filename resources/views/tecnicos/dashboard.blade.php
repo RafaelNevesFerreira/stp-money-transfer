e15aa81d2721425454e2f1469776a3899e3a7618
rafael ferreira <contact@ferreirarafael.com>
2022-04-27 11:02:54 +0200

    .

--- resources/views/tecnicos/dashboard.blade.php
+++ resources/views/tecnicos/dashboard.blade.php
@@ -38,7 +38,7 @@
                                                     <div class="card-body text-center">
                                                         <i class="dripicons-calendar text-muted" style="font-size: 24px;"></i>
                                                         <h3><span>29</span></h3>
-                                                        <p class="text-muted font-15 mb-0">Total Projects</p>
+                                                        <p class="text-muted font-15 mb-0">Hoje</p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -48,7 +48,7 @@
                                                     <div class="card-body text-center">
                                                         <i class=" dripicons-warning text-muted" style="font-size: 24px;"></i>
                                                         <h3><span>715</span></h3>
-                                                        <p class="text-muted font-15 mb-0">Total Tasks</p>
+                                                        <p class="text-muted font-15 mb-0">Canceladas</p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -56,9 +56,9 @@
                                             <div class="col-sm-6 col-lg-3">
                                                 <div class="card shadow-none m-0 border-start">
                                                     <div class="card-body text-center">
-                                                        <i class="dripicons-user-group text-muted" style="font-size: 24px;"></i>
+                                                        <i class="dripicons-broadcast text-muted" style="font-size: 24px;"></i>
                                                         <h3><span>31</span></h3>
-                                                        <p class="text-muted font-15 mb-0">Members</p>
+                                                        <p class="text-muted font-15 mb-0">Recebido</p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -68,7 +68,7 @@
                                                     <div class="card-body text-center">
                                                         <i class="dripicons-graph-line text-muted" style="font-size: 24px;"></i>
                                                         <h3><span>93%</span> <i class="mdi mdi-arrow-up text-success"></i></h3>
-                                                        <p class="text-muted font-15 mb-0">Productivity</p>
+                                                        <p class="text-muted font-15 mb-0">A Receber</p>
                                                     </div>
                                                 </div>
                                             </div>
