0cf5d9d5b231c8afdbf20cfa126b35cfcce509f1
rafael ferreira <contact@ferreirarafael.com>
2022-04-27 12:12:30 +0200

    .

--- resources/views/tecnicos/footer.blade.php
+++ resources/views/tecnicos/footer.blade.php
@@ -5,13 +5,13 @@
                             <div class="col-md-6">
                                 <script>
                                     document.write(new Date().getFullYear())
-                                </script> © Hyper - Coderthemes.com
+                                </script> © {{env("APP_NAME")}} - {{env("APP_URL")}}
                             </div>
                             <div class="col-md-6">
                                 <div class="text-md-end footer-links d-none d-md-block">
-                                    <a href="javascript: void(0);">About</a>
-                                    <a href="javascript: void(0);">Support</a>
-                                    <a href="javascript: void(0);">Contact Us</a>
+                                    <a href="{{route("about")}}">About</a>
+                                    <a href="{{route("help")}}">Support</a>
+                                    <a href="{{route("contact")}}">Contact</a>
                                 </div>
                             </div>
                         </div>
