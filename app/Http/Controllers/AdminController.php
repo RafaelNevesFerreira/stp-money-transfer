6a31178bd1c7f55d1e84a4110a02ddb16d799a3f
rafael ferreira <contact@ferreirarafael.com>
2022-04-29 13:53:44 +0200

    .

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -58,7 +58,16 @@ class AdminController extends Controller
     public function dashboard_stripe()
     {
         $stripe = new \Stripe\StripeClient("sk_test_51JZwMrFzWXjclIq0uBjHEYo8XhVtSEQhe8eJ4Dt6Zwr7igTQ2p3MwIeUQ2RJgMtmAxBRCV6KAo5nJHYlGyoikr4s00T9dLQnId");
-        dd($stripe->paymentIntents->all(['limit' => 100]));
+        $customers = $stripe->customers->all(['limit' => 3]);
+        // dd($customers->count());
+
+        // dd($stripe->customers->all(['limit' => 3]));
+        // foreach ($customers->autoPagingIterator() as $customer) {
+        //     echo "<br>";
+        //     echo $customer->name ;
+        // }
+
+        // dd("done");
         return view("admin.dashboard-stripe");
     }
 }
