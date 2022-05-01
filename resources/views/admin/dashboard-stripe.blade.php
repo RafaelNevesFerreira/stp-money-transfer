fa049c9353bc047a547f1961ca6fb16c433a5770
rafael ferreira <contact@ferreirarafael.com>
2022-05-01 14:44:43 +0200

    ;

--- resources/views/admin/dashboard-stripe.blade.php
+++ resources/views/admin/dashboard-stripe.blade.php
@@ -14,22 +14,13 @@
                     <div class="col-12">
                         <div class="page-title-box">
                             <div class="page-title-right">
-                                <form class="d-flex">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control form-control-light" id="dash-daterange">
-                                        <span class="input-group-text bg-primary border-primary text-white">
-                                            <i class="mdi mdi-calendar-range font-13"></i>
-                                        </span>
-                                    </div>
-                                    <a href="javascript: void(0);" class="btn btn-primary ms-2">
-                                        <i class="mdi mdi-autorenew"></i>
-                                    </a>
-                                    <a href="javascript: void(0);" class="btn btn-primary ms-1">
-                                        <i class="mdi mdi-filter-variant"></i>
-                                    </a>
-                                </form>
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">{{env("APP_NAME")}}</a></li>
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboards</a></li>
+                                    <li class="breadcrumb-item active">Dashboard 2</li>
+                                </ol>
                             </div>
-                            <h4 class="page-title">Dashboard</h4>
+                            <h4 class="page-title">Dashboard 2</h4>
                         </div>
                     </div>
                 </div>
