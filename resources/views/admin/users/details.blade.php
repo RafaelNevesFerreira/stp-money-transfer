98fdbac643c0f06a2d51597eaf88fe14caeedb51
rafael ferreira <contact@ferreirarafael.com>
2022-05-01 20:08:44 +0200

    v

--- resources/views/admin/users/details.blade.php
+++ resources/views/admin/users/details.blade.php
@@ -201,94 +201,95 @@
 
                     </div>
                     <!-- end col -->
-                    <div class="row">
-                        <div class="card col-sm-12">
-                            <div class="card-body">
-                                <h4 class="header-title mb-3">Transações </h4>
 
-                                <div class="table-responsive">
-                                    <table id="products-datatable"
-                                        class="table table-centered table-borderless table-hover w-100 dt-responsive nowrap">
-                                        <thead class="table-light">
+
+                </div>
+                <div class="row">
+                    <div class="card col-sm-12">
+                        <div class="card-body">
+                            <h4 class="header-title mb-3">Transações do usuario {{ $user->name }}</h4>
+
+                            <div class="table-responsive">
+                                <table id="products-datatable"
+                                    class="table table-centered table-borderless table-hover w-100 dt-responsive nowrap">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th style="width: 20px;">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" id="customCheck1">
+                                                    <label class="form-check-label" for="customCheck1">&nbsp;</label>
+                                                </div>
+                                            </th>
+                                            <th>De</th>
+                                            <th>Para</th>
+                                            <th>Valor enviado</th>
+                                            <th>Codigo</th>
+                                            <th>Status</th>
+                                            <th>Enviado</th>
+                                            <th style="width: 75px;">Ação</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($transfers as $transfer)
                                             <tr>
-                                                <th style="width: 20px;">
+                                                <td>
                                                     <div class="form-check">
-                                                        <input type="checkbox" class="form-check-input" id="customCheck1">
-                                                        <label class="form-check-label" for="customCheck1">&nbsp;</label>
+                                                        <input type="checkbox" class="form-check-input"
+                                                            id="customCheck2">
+
                                                     </div>
-                                                </th>
-                                                <th>De</th>
-                                                <th>Para</th>
-                                                <th>Valor enviado</th>
-                                                <th>Codigo</th>
-                                                <th>Status</th>
-                                                <th>Enviado</th>
-                                                <th style="width: 75px;">Ação</th>
+                                                </td>
+                                                <td class="table-user">
+                                                    <a href="javascript:void(0);" class="text-body fw-semibold">
+                                                        {{ $transfer->name }}
+                                                    </a>
+                                                </td>
+
+                                                <td>
+                                                    <span
+                                                        class="fw-semibold">{{ $transfer->destinatary_name }}</span>
+                                                </td>
+                                                <td>
+                                                    @if ($transfer->currency === 'eur')
+                                                        {{ number_format($transfer->value_sended * (int)env('EUR_CAMBIO_VALUE'),2,",",".") }}
+                                                    @elseif ($transfer->currency === 'usd')
+                                                        {{ number_format($transfer->value_sended * (int)env("USD_CAMBIO_VALUE"),2,",",".") }}
+                                                    @else
+                                                        {{ number_format($transfer->value_sended * (int)env("GBP_CAMBIO_VALUE"),2,",",".") }}
+                                                    @endif
+
+                                                </td>
+                                                <td>
+                                                    {{ $transfer->transfer_code }}
+                                                </td>
+
+                                                <td>
+                                                    @if ($transfer->status === 'received')
+                                                        <span class="badge badge-success-lighten">Recebido</span>
+                                                    @elseif ($transfer->status === 'sended')
+                                                        <span class="badge badge-warning-lighten">A receber</span>
+                                                    @else
+                                                        <span class="badge badge-danger-lighten">Reemborsado</span>
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    {{ $transfer->created_at }}
+                                                </td>
+
+                                                <td>
+                                                    <a href="{{ route("admin.transaction.details",$transfer->id)}}" class="action-icon"> <i
+                                                            class="mdi mdi-square-edit-outline"></i>
+                                                    </a>
+                                                </td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            @forelse ($transfers as $transfer)
-                                                <tr>
-                                                    <td>
-                                                        <div class="form-check">
-                                                            <input type="checkbox" class="form-check-input"
-                                                                id="customCheck2">
+                                        @empty
+                                        @endforelse
 
-                                                        </div>
-                                                    </td>
-                                                    <td class="table-user">
-                                                        <a href="javascript:void(0);" class="text-body fw-semibold">
-                                                            {{ $transfer->name }}
-                                                        </a>
-                                                    </td>
-
-                                                    <td>
-                                                        <span
-                                                            class="fw-semibold">{{ $transfer->destinatary_name }}</span>
-                                                    </td>
-                                                    <td>
-                                                        @if ($transfer->currency === 'eur')
-                                                            {{ number_format($transfer->value_sended * (int)env('EUR_CAMBIO_VALUE'),2,",",".") }}
-                                                        @elseif ($transfer->currency === 'usd')
-                                                            {{ number_format($transfer->value_sended * (int)env("USD_CAMBIO_VALUE"),2,",",".") }}
-                                                        @else
-                                                            {{ number_format($transfer->value_sended * (int)env("GBP_CAMBIO_VALUE"),2,",",".") }}
-                                                        @endif
-
-                                                    </td>
-                                                    <td>
-                                                        {{ $transfer->transfer_code }}
-                                                    </td>
-
-                                                    <td>
-                                                        @if ($transfer->status === 'received')
-                                                            <span class="badge badge-success-lighten">Recebido</span>
-                                                        @elseif ($transfer->status === 'sended')
-                                                            <span class="badge badge-warning-lighten">A receber</span>
-                                                        @else
-                                                            <span class="badge badge-danger-lighten">Reemborsado</span>
-                                                        @endif
-                                                    </td>
-                                                    <td>
-                                                        {{ $transfer->created_at }}
-                                                    </td>
-
-                                                    <td>
-                                                        <a href="{{ route("admin.transaction.details",$transfer->id)}}" class="action-icon"> <i
-                                                                class="mdi mdi-square-edit-outline"></i>
-                                                        </a>
-                                                    </td>
-                                                </tr>
-                                            @empty
-                                            @endforelse
-
-                                        </tbody>
-                                    </table>
-                                </div> <!-- end table responsive-->
-                            </div> <!-- end col-->
-                        </div> <!-- end row-->
-                    </div>
-
+                                    </tbody>
+                                </table>
+                            </div> <!-- end table responsive-->
+                        </div> <!-- end col-->
+                    </div> <!-- end row-->
                 </div>
                 <!-- end row -->
 
