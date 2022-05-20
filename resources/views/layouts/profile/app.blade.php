f42eb1c097695a16c96c142a0504851c7aa351dc
rafael ferreira <contact@ferreirarafael.com>
2022-05-20 17:16:59 +0200

    .

--- resources/views/layouts/profile/app.blade.php
+++ resources/views/layouts/profile/app.blade.php
@@ -170,7 +170,21 @@
                     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
                     var yyyy = today.getFullYear();
                     today = dd + '/' + mm + '/' + yyyy;
+                    number_format = function(number, decimals, dec_point, thousands_sep) {
+                        number = number.toFixed(decimals);
 
+                        var nstr = number.toString();
+                        nstr += '';
+                        x = nstr.split('.');
+                        x1 = x[0];
+                        x2 = x.length > 1 ? dec_point + x[1] : '';
+                        var rgx = /(\d+)(\d{3})/;
+
+                        while (rgx.test(x1))
+                            x1 = x1.replace(rgx, '$1' + thousands_sep + '$2');
+
+                        return x1 + x2;
+                    }
 
                     switch (data["currency"]) {
                         case "eur":
