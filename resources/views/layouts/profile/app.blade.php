439978b2c5a0bbc8c2400a58b62a42b9f8f3f587
rafael ferreira <contact@ferreirarafael.com>
2022-05-20 17:27:42 +0200

    .

--- resources/views/layouts/profile/app.blade.php
+++ resources/views/layouts/profile/app.blade.php
@@ -227,6 +227,7 @@
 
                     var total = data["value_sended"] + data["tax"];
 
+                    console.log(typeof total, total);
                     $("#transfer_value").text(number_format(total, 2, ",",
                         ".") + currency)
                     $("#transfer_date").text(today)
