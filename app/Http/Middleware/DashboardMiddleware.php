5036e17f35b405cc3e54a4deb1260438db4f1d4e
rafael ferreira <contact@ferreirarafael.com>
2022-05-20 17:08:06 +0200

    .

--- app/Http/Middleware/DashboardMiddleware.php
+++ app/Http/Middleware/DashboardMiddleware.php
@@ -19,14 +19,12 @@ class DashboardMiddleware
     {
         if (Auth::check() && Auth::user()->role === "1") {
             return redirect()->route("profile.dashboard");
-        } elseif (Auth::check() && Auth::user()->role === "2") {
+        } else if (Auth::check() && Auth::user()->role === "2") {
             return redirect()->route("tecnico.dashboard");
-        } elseif (Auth::check() && Auth::user()->role === "3") {
+        } else if (Auth::check() && Auth::user()->role === "3") {
             return redirect()->route("admin.dashboard");
-        } elseif (!Auth::check()) {
+        } else if (!Auth::check()) {
             return redirect()->route("login");
-        } else {
-            return redirect()->route("home");
         }
     }
 }
