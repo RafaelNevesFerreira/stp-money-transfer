4067d96778a5e3cb8b5b642418087a3213038677
rafael ferreira <contact@ferreirarafael.com>
2022-05-20 21:00:17 +0200

    .

--- app/Repositories/Eloquent/PostsRepository.php
+++ app/Repositories/Eloquent/PostsRepository.php
@@ -17,9 +17,9 @@ class PostsRepository extends AbstractRepository implements PostsRepositoryInter
     {
         return $this->model::query()->with('tags')
         ->when(request()->has("s"), function ($query) {
-            $query->whereHas("tags", function ($query) {
+            // $query->whereHas("tags", function ($query) {
                 $query->where("title", "like", "%" . request("s") . "%");
-            });
+            // });
         })->orderBy('created_at', 'desc')->paginate(6);;
     }
 }
