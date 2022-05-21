815f9307e3b748598be320b59f99125876cf09cc
rafael ferreira <contact@ferreirarafael.com>
2022-05-21 14:52:23 +0200

    .

---
+++ lang/pt_BR/card_error.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Your card has insufficient funds.'   => 'Seu cartão não tem fundos suficientes.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+];
